c4682f21a7f5e9bb36f2acc248f4de7967f19e50
emchamp <lightingthefuse@gmail.com>
2022-12-16 10:46:18 +0800

    fix vul 2

--- src/login.js
+++ src/login.js
@@ -1,8 +1,8 @@
 var db = require('../models');
 
 module.exports.userSearch = function (req, res) {
-  // var sqlq1 = "SELECT * from user where id=2";
-  var sqlq1 = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
+  var sqlq1 = "SELECT * from user where id=2";
+  //var sqlq1 = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
 	db.sequelize.query(sqlq1, {
 		model: db.User
 	}).then(user => {
