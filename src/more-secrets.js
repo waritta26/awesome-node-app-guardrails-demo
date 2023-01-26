3644ceade6c1531e2d93aa036a2492a5a1b2e51c
emchamp <lightingthefuse@gmail.com>
2023-01-26 13:51:08 +0800

    eliminate secrets

--- src/more-secrets.js
+++ src/more-secrets.js
@@ -1,27 +1,2 @@
 var AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
 var AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-var PWD = "askldfjlsadf";
-var API_KEY = "sdfasdf";
-var access_key = "sdfsdf";
-var AWS_SECRET_ACCESS_KEY = "TestKeyPassPwd";
-var DSA_PRIVATE_KEY = "sdkflasaskdjfhjksdhfkdsjf";
-var password = "thisismypassword";
-var AWS_SECRET_KEY = "TestKeyPassPwasdlfsdfasdfd";
-var DSA_PRIVATE_KEY = "My Private Key is here";
-var git_token = "3cb43625bb5a9a7f58498517asdsadas7sa7d687a5sd78sa6d";
-var gpg_key = "8sa7d8sa7d0sa7dsa7d98sad7sa6d7sa6d98sads8a7d0sad7sa87d89sa7d98sa7d98sa7d8sad";
-var key = "4dsf245dsf896ds8f59ds65f4fdf587F5ds8dsf95ds87f";
-
-var ssh_key = "Y2QwMWU0ZWUtOWZkMy00YjIxLThkMmYtMmVkYWFhZTBhMDI2OjEwZmYxMTQ3LWY4ZTAtNDU3ZC05ZGRlLWZhMWMzN2E1MTyuMA==";
-var openssh_private_key = "mytest";
-var SUMMVED_Password = "Slsdfl";
-var AKC_TOKEN = "sakdfjlsdf";
-var summved = 'test';
-var summved2 = "testing232183test";
-var test_keys = 'jsabdsafdsfjkbdsakfsbdlf';
-var har_har_mahadev = 'lknajankjclbsaljdf765943534m5n4b6';
-var har_har = 'lknajankjclbsaljdf765943534m5n4b6';
-var har_hars = 'lknajankjclbsaljdf765943534m5n4b6';
-var hars = 'lknajankjclbsaljdf765943534m5n4b6';
-var mahardev = 'lknajankjclbsaljdf765943534m5n4b6';
-var hars_hars = 'lknajankjclbsaljdf765943534m5n4b6';
