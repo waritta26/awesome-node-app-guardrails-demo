4ffc4e3f2460cd3525a4424e13a80f487b86c219
emchamp <lightingthefuse@gmail.com>
2023-02-08 11:11:48 +0800

    fix vulnerabilities

--- src/login.js
+++ src/login.js
@@ -1,9 +1,11 @@
 var db = require('../models');
 
 module.exports.userSearch = function (req, res) {
-	var sqlq1 = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
-	db.sequelize.query(sqlq1, {
-		model: db.User
+	var sqlq1 = "SELECT name,id FROM Users WHERE login=:login";
+	db.sequelize.query(sqlq1, 
+		{
+            raw: true,
+            replacements: { email: req.body.email }
 	}).then(user => {
 		if (user.length) {
 			var output = {
