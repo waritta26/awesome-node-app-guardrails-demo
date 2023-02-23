2f8c0c8035a0cf108eed355cc7c9b388f31cfcbc
waritta26 <waritta.s.2009@gmail.com>
2023-02-23 11:31:04 +0700

    Update login.js

--- src/login.js
+++ src/login.js
@@ -3,8 +3,8 @@ var db = require('../models');
 module.exports.userSearch = function (req, res) {
 	var sqlq1 = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
 	db.sequelize.query(sqlq1, {
-		model: db.User
-	}).then(user => {
+  	raw: true,
+  	replacements: { email: req.body.email }).then(user => {
 		if (user.length) {
 			var output = {
 				user: {
